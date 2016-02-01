99c168497e6c68563d4ff2fa40b813020ec6eaf6
Olivier Chafik <ochafik@google.com>
2016-02-01 22:55:53 +0000

    tree-shake example

--- test/codegen/expect/tree_shaking.js
+++ test/codegen/expect/tree_shaking.js
@@ -5,13 +5,6 @@ dart_library.library('tree_shaking', null, /* Imports */[
 ], function(exports, dart, core) {
   'use strict';
   let dartx = dart.dartx;
-  class _Drop1 extends core.Object {}
-  function _drop2() {
-  }
-  dart.fn(_drop2);
-  exports._drop3 = null;
-  exports._drop4 = 0;
-  const _drop5 = 0;
   class _Keep1 extends core.Object {}
   function _keep2() {
   }
@@ -19,30 +12,21 @@ dart_library.library('tree_shaking', null, /* Imports */[
   exports._keep3 = null;
   exports._keep4 = 0;
   const _keep5 = 0;
-  const _drop1 = Symbol('_drop1');
   const _keep1 = Symbol('_keep1');
   class _Foo extends core.Object {
     _Foo() {
     }
-    dropped() {
-    }
     keep() {
     }
-    [_drop1]() {}
     [_keep1]() {}
   }
-  dart.defineNamedConstructor(_Foo, 'dropped');
   dart.defineNamedConstructor(_Foo, 'keep');
   dart.setSignature(_Foo, {
     constructors: () => ({
       _Foo: [_Foo, []],
-      dropped: [_Foo, []],
       keep: [_Foo, []]
     }),
-    methods: () => ({
-      [_drop1]: [dart.dynamic, []],
-      [_keep1]: [dart.dynamic, []]
-    })
+    methods: () => ({[_keep1]: [dart.dynamic, []]})
   });
   function main() {
     new _Keep1();
