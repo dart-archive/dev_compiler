ae385f68516caa5315e2db5d810de1bda09f9f7a
Olivier Chafik <ochafik@google.com>
2016-02-02 16:09:52 +0000

    Move tree shaking example down

--- tool/tree_shaking_test.sh
+++ tool/tree_shaking_test.sh
@@ -5,17 +5,23 @@ cd $( dirname "${BASH_SOURCE[0]}" )/..
 
 [[ -d example/tree_shaking ]] || mkdir -p example/tree_shaking
 
-./tool/build_sdk.sh \
-  --tree-shaking=all \
-  --destructure-named-params \
-  --modules=node \
-  -o example/tree_shaking \
-  test/codegen/language/hello_dart_test.dart \
-  test/codegen/language/hello_dart_test.html
+function compile_and_run() {
+  local file=$1
+  local name=`basename $file | sed 's/\.dart$//'`
 
-NODE_PATH=example/tree_shaking \
-  node \
-  --harmony \
-  --harmony_destructuring \
-  --harmony_default_parameters \
-  -e "require('dart/_isolate_helper').startRootIsolate(require('hello_dart_test').main, []);"
+  ./tool/build_sdk.sh \
+    --tree-shaking=all \
+    --destructure-named-params \
+    --modules=node \
+    -o example/$name \
+    $file
+
+  NODE_PATH=example/$name \
+    node \
+    --harmony \
+    --harmony_destructuring \
+    --harmony_default_parameters \
+    -e "require('dart/_isolate_helper').startRootIsolate(require('$name').main, []);"
+}
+
+compile_and_run test/codegen/language/hello_dart_test.dart
