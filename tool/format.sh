e7d4dd30ebd2d3c9ff0fecff4028b46b6568ed8c
Olivier Chafik <ochafik@google.com>
2016-02-04 22:13:14 +0000

    fail travis when formatter finds anything

--- tool/format.sh
+++ tool/format.sh
@@ -10,7 +10,26 @@ cd $( dirname "${BASH_SOURCE[0]}" )/..
 #   * local files that have never been added to git,
 #   * subdirectories of test/ and tool/, unless explicitly added. Those dirs
 #     contain a lot of generated or external source we should not reformat.
-(files=`git ls-files 'bin/*.dart' 'lib/*.dart' test/*.dart test/checker/*.dart \
-  tool/*.dart | grep -v lib/src/js/`; git status -s $files | grep -q . \
-  && echo "Did not run the formatter, please commit edited files first." \
-  || (echo "Running dart formatter" ; pub run dart_style:format -w $files))
+file_patterns=(
+  'bin/*.dart'
+  'lib/*.dart'
+  test/*.dart
+  test/checker/*.dart
+  tool/*.dart
+)
+files=`git ls-files "${file_patterns[@]}" | grep -v lib/src/js/`
+echo "FILES: ${files[*]}"
+
+if git status -s $files | grep -q . ; then
+  echo "Did not run the formatter, please commit edited files first."
+  exit 1
+else
+  echo "Running dart formatter"
+  pub run dart_style:format -w $files > format.log
+  if [[ "$TRAVIS" == "true" ]]; then
+    if cat format.log | grep -q Formatted ; then
+      echo "Found files that need formatting: please run the presubmit tests before pushing"
+      exit 1
+    fi
+  fi
+fi
