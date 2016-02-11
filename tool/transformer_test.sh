549bd02c1083787153a2405359ec8133b664630a
Olivier Chafik <ochafik@google.com>
2016-02-11 17:40:58 +0000

    don't output closure types

--- tool/transformer_test.sh
+++ tool/transformer_test.sh
@@ -2,7 +2,7 @@
 set -eu
 DIR=$(dirname "${BASH_SOURCE[0]}")
 
-if [[ "$TRAVIS" != "true" ]]; then
+if [[ "${TRAVIS:-false}" != "true" ]]; then
   echo "*** Testing pub serve + DDC transformer"
   pub run test --timeout 120s test/transformer_e2e_test.dart
 fi
