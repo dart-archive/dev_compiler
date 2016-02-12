34dbba354eedb987c1a5faa4c9faa431d4299ada
Olivier Chafik <ochafik@google.com>
2016-02-12 13:32:53 +0000

    just run transformer tests for easier debugging

--- tool/presubmit.sh
+++ tool/presubmit.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 DIR=$(dirname "${BASH_SOURCE[0]}")
-$DIR/build_sdk.sh
-$DIR/test.sh
-$DIR/browser_test.sh
-$DIR/node_test.sh
-$DIR/analyze.sh
-$DIR/format.sh
+# $DIR/build_sdk.sh
+# $DIR/test.sh
+# $DIR/browser_test.sh
+# $DIR/node_test.sh
+# $DIR/analyze.sh
+# $DIR/format.sh
 $DIR/transformer_test.sh
